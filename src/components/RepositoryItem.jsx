ac81d217e3587efb92185c574e5b3835f2e167c7
Marcus Botelho <marcus.botelho@dxtcps004203.local>
2021-03-11 21:57:21 -0300

    fetching datas and add styles

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -1,9 +1,9 @@
-export function RepositoryItem({ repository, description, link }) {
+export function RepositoryItem({ repository }) {
   return (
     <li>
-      <strong>{repository ?? "Default"}</strong>
-      <p>{description ?? "Descrição indisponível"}</p>
-      <a href={link}>Acessar repositório</a>
+      <strong>{repository.name ?? "Default"}</strong>
+      <p>{repository.description ?? "Descrição indisponível"}</p>
+      <a href={repository.html_url}>Acessar repositório</a>
     </li>
   );
 }
--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,26 +1,25 @@
-import { Counter } from "./Counter";
+import { useState, useEffect } from "react";
 import { RepositoryItem } from "./RepositoryItem";
 
+import "../styles/repositories.scss";
+
 export function RepositoryList() {
-  const repos = {
-    name: "Unform",
-    description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-      Ut hendrerit magna dignissim justo posuere, sed laoreet velit euismod.
-      Suspendisse orci nisi, vehicula vitae ultricies in, molestie nec nunc.
-      Etiam mattis, ipsum ut pharetra varius, nibh mi vehicula erat, vitae
-      molestie enim leo sed risus.`,
-    link: "https://github.com/unform/unform",
-  };
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.github.com/orgs/rocketseat/repos")
+      .then((response) => response.json())
+      .then((data) => setRepositories(data));
+  }, []);
 
   return (
     <section className="repository-list">
       <h1>Lista de repositórios</h1>
       <ul>
-        <RepositoryItem
-          repository={repos.name}
-          description={repos.description}
-          link={repos.link}
-        />
+        {repositories &&
+          repositories.map((repository) => (
+            <RepositoryItem key={repository.name} repository={repository} />
+          ))}
       </ul>
     </section>
   );
